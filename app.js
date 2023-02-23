4da144765c4f14d55f6c4b00277006dcece85cf5
Ruslan <20355378+frqher@users.noreply.github.com>
2023-02-23 15:30:31 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ const e_div_cavab_body = document.getElementById("cavab-card-body");
 const e_sinif = document.getElementById("select_sinif");
 const e_unit = document.getElementById("select_unit");
 const e_dil = document.getElementById("select_dil");
+const e_zorluk = document.getElementById("select_cetinlik");
+const e_div_zorluk = document.getElementById("zorluk_2");
 const e_baslat = document.getElementById("start");
 const e_yoxla = document.getElementById("check");
 const e_sual = document.getElementById("sual");
+const e_card_info = document.getElementById("kart_info");
 const e_cavab = document.getElementById("cavab");
 const e_cavab_text = document.getElementById("sual_cavab");
 const e_cavab_goster = document.getElementById("show_answer");
+const e_variantlar = document.querySelectorAll(".variant");
 
 let u_sinif;
 let u_unit;
 let u_dil;
 let u_dil_2;
+let u_cetinlik;
 let u_sual;
 let u_index;
+let u_cevap;
 let objects;
 
 eventListeners();
@@ -23,15 +29,34 @@ eventListeners();
 function eventListeners(){
     
     e_baslat.addEventListener("click", checkTest);
-    e_yoxla.addEventListener("click", checkAnswer);
-    e_cavab_goster.addEventListener("click", showAnswer);
-    e_cavab.addEventListener("keydown", (event) => {
-        if (event.key == "Enter"){
+    e_yoxla.addEventListener("click", () => {
+        if (u_cetinlik == 2){
+            u_cevap = e_cavab.value.toLowerCase().trim();
             checkAnswer();
         }
     });
+    e_cavab_goster.addEventListener("click", showAnswer);
+    e_div_zorluk.addEventListener("click", (e) => {
+        if (e.target.className == "btn btn-primary variant"){
+            if (u_cetinlik == 1){
+                u_cevap = e.target.textContent.toLowerCase().trim();
+                checkAnswer();
+            }
+        }
+    });
+    e_cavab.addEventListener("keydown", (event) => {
+        if (event.key == "Enter"){
+
+            if (u_cetinlik == 2){
+                u_cevap = e_cavab.value.toLowerCase().trim();
+                checkAnswer();
+            }
+        }
+    });
+
 
 }
+
 
 function showAnswer(e){
     let durum = e_cavab_goster.textContent;
@@ -58,8 +83,12 @@ function incorrectAnswer(){
     UI.errorMsg("cavab-card-body", "danger", "Cavab düzgün deyil");
 }
 
+function getAnswer(answer){
+    return u_cevap;
+}
+
 function checkAnswer(){
-    let cevap = e_cavab.value.toLowerCase().trim();
+    let cevap = getAnswer();
     let soru = u_sual[u_dil_2].split(",");
     let answer_durum = false;
 
@@ -80,6 +109,10 @@ function checkAnswer(){
 
 function startTest(){
     u_sual = getRandomSual();
+    let fake_suallar = getRandomFakeSual();
+    let real_sual_random_pos = Math.floor(Math.random() * 4);
+    let real_sual = u_sual[u_dil_2].split(",")[0];
+    fake_suallar.splice(real_sual_random_pos, 0, real_sual);
 
     if (u_sual == -1){
         e_sual.textContent = "Bütün sualları düzgün cavabladın, mükemmelsen !";
@@ -91,8 +124,10 @@ function startTest(){
         e_sual.textContent = "Sual: ";
 
         let soru_uzunluk = u_sual[u_dil].split(",").length - 1;
-
+        // e_variantlar[0].textContent
+        
         u_sual[u_dil].split(",").forEach((element, index) => {
+            
             if (soru_uzunluk == index ){
                 e_sual.textContent = e_sual.textContent  + element;
             }
@@ -100,6 +135,21 @@ function startTest(){
                 e_sual.textContent = e_sual.textContent  + element + ", ";
             }
         });
+
+        if(u_cetinlik == 1){
+            e_yoxla.classList.add("d-none");
+            e_cavab.classList.add("d-none");
+            e_div_zorluk.classList.remove("d-none");
+
+            fake_suallar.forEach((element, index) => {
+                e_variantlar[index].textContent = element;
+            });
+        }
+        else{
+            e_yoxla.classList.remove("d-none");
+            e_cavab.classList.remove("d-none");
+            e_div_zorluk.classList.add("d-none");
+        }
         
     }
 
@@ -121,6 +171,17 @@ function getRandomSual(){
     
 }
 
+function getRandomFakeSual(){
+    let fake_suallar = [];
+
+    for (let index = 1; index < 5; index++) {
+        let sual = objects[getRandomNumber()][u_dil_2].split(",");
+        fake_suallar.push(sual[0]);
+    }
+
+    return fake_suallar;
+}
+
 function removeSual(number){
     objects.splice(number, 1)
     e_cavab.value = ""
@@ -134,28 +195,50 @@ function getRandomNumber(){
     return random_number;
 }
 
+function setLanguage(dil){
+
+    if (dil == "Azərbaycan"){
+        u_dil = 0
+        u_dil_2 = 1
+    } 
+    else if (dil == "İngilis"){
+        u_dil = 1
+        u_dil_2 = 0
+    }
+
+}
+
+function setDifficult(zorluk){
+
+    if (zorluk == "Asan"){
+        u_cetinlik = 1
+        e_card_info.textContent = "Doğru Cavabı Seçin";
+    }
+    else if (zorluk == "Çətin"){
+        u_cetinlik = 2
+        e_card_info.textContent = "Doğru Cavabı Yazın";
+    }
+
+}
+
 function checkTest(){
 
     let error_select = "Sec...";
     let e_sinif_v = e_sinif.options[e_sinif.selectedIndex].text;
     let e_unit_v = e_unit.options[e_unit.selectedIndex].text;
     let e_dil_v = e_dil.options[e_dil.selectedIndex].text;
+    let e_cetinlik = e_zorluk.options[e_zorluk.selectedIndex].text;
 
-    if (e_sinif_v == error_select || e_unit_v == error_select || e_dil_v == error_select){
-        UI.errorMsg("category", "danger", "Sinif, Unit ve Dili seçin!");
+    if (e_sinif_v == error_select || e_unit_v == error_select || e_dil_v == error_select || e_cetinlik == error_select){
+        UI.errorMsg("category", "danger", "Sinif, Unit, Dil ve Çətinlik səviyyəsini seçin!");
     }
     else{
+
+        setLanguage(e_dil_v)
+        setDifficult(e_cetinlik)
+
         u_sinif = e_sinif_v;
         u_unit = e_unit_v;
-        
-        if (e_dil_v == "Azərbaycan"){
-            u_dil = 0
-            u_dil_2 = 1
-        } 
-        else if (e_dil_v == "İngilis"){
-            u_dil = 1
-            u_dil_2 = 0
-        }
 
         e_cavab_goster.textContent = "Cavabı Göstər";
         e_cavab_text.textContent = "Cavab:*****";
